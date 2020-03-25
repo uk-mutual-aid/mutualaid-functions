0507963a903a0220a06c80e286cc63edc4729e86
Nazreen Mohamad <nxmohamad@gmail.com>
2020-03-25 01:54:58 +0000

    log body

--- src/functions/index.js
+++ src/functions/index.js
@@ -4,6 +4,6 @@ const functions = require('firebase-functions')
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.volunteerSignUp = functions.https.onRequest((request, response) => {
-  console.log(request)
+  console.log(request.body)
   response.send('Hello from Firebase!')
 })
