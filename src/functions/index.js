031d971f8645acc9d9aed482db0279b5594d7439
Nazreen Mohamad <nxmohamad@gmail.com>
2020-03-27 00:18:59 +0000

    remove comment

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,9 +1,6 @@
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.volunteerSignUp = functions.https.onRequest((request, response) => {
   const { body, method, params } = request
   console.log({ body, method, params })
