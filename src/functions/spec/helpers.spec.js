44e5752033ef423ddeffff77098e8bd8245788fa
Nazreen Mohamad <nxmohamad@gmail.com>
2020-05-01 17:27:56 +0100

    spec

--- src/functions/spec/helpers.spec.js
+++ src/functions/spec/helpers.spec.js
@@ -1,5 +1,45 @@
 const helpers = require('../helpers')
 
+describe('parseGoogleFormToSignUp', () => {
+  it('output matches expected schema', () => {
+    const input = {
+    Availability: 'Monday,Tuesday',
+    'Contact number': '07871807220',
+    'Data privacy consent': 'I consent to be contacted by my local Mutual Aid volunteer groups to join the local volunteer response to COVID-19 pandemic.',
+    Discord: 'discord',
+    'Do you have a car?': 'Yes',
+    Email: 'name@email.com',
+    Facebook: 'https://www.facebook.com/groups/215636692879586/',
+    'I am a...': 'Mutual aid group admin',
+    'I speak English and...': 'English, French',
+    Postcode: 'NW1 1QL',
+    'Services offered': 'groceries',
+    Telegram: 'https://telegram.com/test',
+    Whatsapp: 'https://whatsapp.com/test',
+    'Your name': 'John Doe'
+    }
+    const expected = {
+      availability: [ 'Monday', 'Tuesday' ],
+      data_privacy_consent:
+      'I consent to be contacted by my local Mutual Aid volunteer groups to join the local volunteer response to COVID-19 pandemic.',
+      contact_number: '07871807220',
+      discord: 'discord',
+      owns_car: true,
+      email: 'name@email.com',
+      facebook: 'https://www.facebook.com/groups/215636692879586/',
+      roles: [ 'Mutual aid group admin' ],
+      spoken_languages: 'English, French',
+      postcode: 'NW1 1QL',
+      services_offered: 'groceries',
+      telegram: 'https://telegram.com/test',
+      whatsapp: 'https://whatsapp.com/test',
+      name: 'John Doe'
+    }
+    const result = helpers.parseGoogleFormResponseToSignUp(input)
+    expect(result).toEqual(expected)
+  })
+})
+
 describe('parseSignUpToVolunteer:', () => {
   it('output matches expected schema', () => {
     const input = {
