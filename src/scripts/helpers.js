122563708f044dace11ab729750f9db27f9028bd
Nazreen Mohamad <nxmohamad@gmail.com>
2020-05-01 22:38:51 +0100

    update readCsv script

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,13 +1,14 @@
 const fs = require('fs')
 const neatCsv = require('neat-csv');
 
-async function readCsv(path) {
-  fs.readFile(path, async (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log(await neatCsv(data))
+function readCsv(path) {
+  return new Promise((res,rej) => {
+    fs.readFile(path, async (err, data) => {
+      if (err) {
+        return rej(err)
+      }
+      return res(await neatCsv(data))
+    })
   })
 }
 
