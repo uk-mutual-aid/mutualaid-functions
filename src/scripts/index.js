a2b27b9031963582583682beaf3f205f4e4aa550
Nazreen Mohamad <nxmohamad@gmail.com>
2020-05-01 22:38:58 +0100

    prepare all payloads

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,12 @@
 const { readCsv } = require('./helpers')
-
+const { parseGoogleFormResponseToSignUp, parseSignUpToVolunteer, convertVolunteerToGeoJson } = require('../functions/helpers')
 const inputPath = '../tmp/data/1may.csv'
 
 async function main() {
-  const records = await readCsv(inputPath)
-  console.log(records)
+  const rawSignUps = await readCsv(inputPath)
+  const signUpPayloads = rawSignUps.map(parseGoogleFormResponseToSignUp)
+  const volunteerPayloads = signUpPayloads.map(parseSignUpToVolunteer)
+  const volunteerGeoPayloads = await Promise.all(volunteerPayloads.slice(0,1).map(convertVolunteerToGeoJson))
 }
 
 main()
