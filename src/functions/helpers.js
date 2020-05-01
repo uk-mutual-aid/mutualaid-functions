f532fa3e3a8372470a7e895b16229446c84fcbd0
Nazreen Mohamad <nxmohamad@gmail.com>
2020-05-01 17:28:33 +0100

    extract parseGoogleForm

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -1,6 +1,81 @@
 const axios = require('axios')
 const { POSTCODE_LOOKUP_URL } = process.env
 
+function parseGoogleFormResponseToSignUp(input) {
+  const formItemTitleToKeyMap = {
+    Availability: 'availability',
+    'Data privacy consent': 'data_privacy_consent',
+    'Contact number': 'contact_number',
+    Discord: 'discord',
+    'Do you have a car?': 'owns_car',
+    Email: 'email',
+    Facebook: 'facebook',
+    'I am a...': 'roles',
+    'I speak English and...': 'spoken_languages',
+    Postcode: 'postcode',
+    'Services offered': 'services_offered',
+    Telegram: 'telegram',
+    Whatsapp: 'whatsapp',
+    'Your name': 'name'
+  }
+
+  function changeObjectKeys(obj, map) {
+    let result = {}
+    const mapKeys = Object.keys(map)
+    mapKeys.forEach(mapKey => {
+      let value = obj[mapKey]
+      let newKey = map[mapKey]
+      result[newKey] = value
+    })
+    return result
+  }
+
+  function certainStringValuesToArray(targetKeys, object) {
+    /* transforms certain values of the object from string to array, using comma as the separator, based on the keys provided*/
+    const keys = Object.keys(object)
+    keys.forEach(key => {
+      if (targetKeys.includes(key)) {
+        object[key] = object[key].split(',')
+      }
+    })
+    return object
+  }
+
+  const boolQuestionMap = {
+    owns_car: {
+      Yes: true,
+      No: false
+    }
+  }
+
+  function parseBoolQuestionValues(obj, map) {
+    const questionKeys = Object.keys(map)
+    questionKeys.forEach(questionKey => {
+      // assumes that the value is an array
+      const answerArray = obj[questionKey]
+      const firstValue = answerArray[0]
+      const mapObject = map[questionKey]
+      const resultingValue = mapObject[firstValue]
+      obj[questionKey] = resultingValue
+    })
+    return obj
+  }
+
+      // below is a hack because we had to call Array.string() from the Script Editor side to solve the java.lang issue
+      const keysMeantToBeArrays = [
+        'Availability',
+        'I am a...',
+        'Data Privacy Consent',
+        'Do you have a car?'
+      ]
+      // todo: move below to new parseData function
+      let result = certainStringValuesToArray(keysMeantToBeArrays, input)
+      result = changeObjectKeys(result, formItemTitleToKeyMap)
+      result = parseBoolQuestionValues(result, boolQuestionMap)
+      return result
+}
+
+
 function parseSignUpToVolunteer(input, signUpId) {
   const result = {
     availability: input.availability,
@@ -60,6 +135,7 @@ async function convertVolunteerToGeoJson(doc){
 }
 
 module.exports = {
+  parseGoogleFormResponseToSignUp,
   parseSignUpToVolunteer,
   convertVolunteerToGeoJson
 }
